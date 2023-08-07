541ca27d3d73028c7808defdf5468188ea9cb346
Jimmy Signature <jimmysignature@gmail.com>
2023-08-06 21:00:44 -0400

    add search function

--- src/components/AllPlayers.jsx
+++ src/components/AllPlayers.jsx
@@ -14,26 +14,61 @@ const AllPlayers = () => {
   }, [])
 
   const [players, setPlayers] = useState([])
+  const [searchValue, setSearchValue] = useState('')
+  // console.log(searchValue)
   const navigate = useNavigate()
   function handleClick(id) {
     navigate(`/players/${id}`)
     console.log(id)
   }
+  const filteredPlayers = players.filter(player =>
+    player.name.includes(searchValue)
+  )
+
+  console.log(filteredPlayers)
 
   return (
     <>
       <NavBar />
       <div>
-        {players.map(player => (
-          <div key={player.id}>
-            <img src={player.imageUrl} />
-            <p>
-              {player.id}
-              {player.name}
-            </p>
-            <button onClick={() => handleClick(player.id)}>Details</button>
+        <label for="site-search">Search puppies:</label>
+        <input
+          type="search"
+          id="site-search"
+          name="q"
+          onChange={e => {
+            setSearchValue(e.target.value)
+          }}
+        />
+      </div>
+      <div>
+        {searchValue ? (
+          <div>
+            {filteredPlayers.map(player => (
+              <div key={player.id}>
+                <img src={player.imageUrl} />
+                <p>
+                  {player.id}
+                  {player.name}
+                </p>
+                <button onClick={() => handleClick(player.id)}>Details</button>
+              </div>
+            ))}{' '}
           </div>
-        ))}{' '}
+        ) : (
+          <div>
+            {players.map(player => (
+              <div key={player.id}>
+                <img src={player.imageUrl} />
+                <p>
+                  {player.id}
+                  {player.name}
+                </p>
+                <button onClick={() => handleClick(player.id)}>Details</button>
+              </div>
+            ))}{' '}
+          </div>
+        )}
       </div>
     </>
   )
